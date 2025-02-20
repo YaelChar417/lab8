040f09a82c74fc778ec2018bbf6247863de294a0
YaelChar417 <A01711111@tec.mx>
2025-02-20 12:51:36 -0600

    Feature: metodo de ordenamiento 100 real no fake

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const file_system = require('fs');
 
 file_system.writeFileSync('hola.txt', 'holaaaa desde node');
+
+const arreglo = [5000, 1, 19, 258, 417, 88, 999, 1027, 587];
+
+for(let item of arreglo)
+{
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+} 
